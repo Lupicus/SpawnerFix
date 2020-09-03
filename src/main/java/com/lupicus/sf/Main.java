d98d7f01d96a98b7831be7b851855d074d54c9f8
Lupicus <54924432+Lupicus@users.noreply.github.com>
2020-09-02 20:54:38 -0500

    Initial code for 1.16.1

---
+++ src/main/java/com/lupicus/sf/Main.java
@@ -0,0 +1,13 @@
+package com.lupicus.sf;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Main.MODID)
+public class Main
+{
+    public static final String MODID = "sf";
+
+    public Main()
+    {
+    }
+}
