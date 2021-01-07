0ce8d853e42fab37c4163c907c09536333e95bc9
Lupicus <54924432+Lupicus@users.noreply.github.com>
2021-01-07 05:44:44 -0600

    Support ZombifiedPiglin

--- src/main/java/com/lupicus/sf/Main.java
+++ src/main/java/com/lupicus/sf/Main.java
@@ -1,6 +1,11 @@
 package com.lupicus.sf;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(Main.MODID)
 public class Main
@@ -9,5 +14,7 @@ public class Main
 
     public Main()
     {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+				() -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a,b) -> true));
     }
 }
